d3ad2fd8ecb87f1f32a7a248634a8301cde3b564
studyingtactics0-hash <studyingtactics0@gmail.com>
2026-09-12 22:43:36 +0500

    Remove global AuthGuard from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import AuthGuard from "@/components/AuthGuard";
 import Header from "@/components/Header";
 
 const geistSans = Geist({
@@ -55,10 +54,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AuthGuard>
           <Header />
           {children}
-        </AuthGuard>
       </body>
     </html>
   );
